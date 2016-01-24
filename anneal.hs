32db7660f25e71f62c74307f8255b9bcf3add358
John de la Garza <john@jjdev.com>
2016-01-23 17:21:34 -0800

    anneal.hs: greedily move people together

--- anneal.hs
+++ anneal.hs
@@ -1,19 +1,35 @@
-nums = concat [[n | m <- [1..2]] | n <- [1..6]]
---nums = concat [take 3 (repeat x) | x <- [1..10]]
+people = concat [[n | m <- [1..3]] | n <- [1..2]]
+tables = [1..length people]
+placements = (zip people tables) ++ [(100,n) | n <- [1..length tables]]
+--placements = [(1,1),(1,2),(8,3),(9,4), (100,1),(100,2),(100,3),(100,4)]
 
 merge [] ys = ys
 merge (x:xs) ys = x:merge ys xs
 
-shuffle xs n
-        | n > 0  = shuffle (merge (reverse a) b) (n-1)
+shuffle' xs n
+        | n > 0  = shuffle' (merge (reverse a) b) (n-1)
         | otherwise = xs
     where (a,b) = splitAt (quot (length xs) 2) xs
+shuffle xs = shuffle' xs (quot (length xs) 2)
 
-anneal world n
-    | n > 0 = anneal world (n-1)
-    | otherwise = world
+anneal [] new_p = reverse new_p
+anneal ((p,t):ps) new_p =
+    anneal ps $ bestNeighbor:new_p
+  where neighbors = filter (\(x,y)->y>=t-1&&y<=t+1) (ps++new_p)
+        bestNeighbor = if p == 100 then (p,t) else best neighbors (p,t)
+
+best' [] person acc = (fst person, snd acc)
+best' ((p1,t1):ps) (p,t) acc
+    | abs (p-p1) < abs (p-(fst acc)) = best' ps (p,t) (p1,t1)
+    | otherwise = best' ps (p,t)  acc
+best neighbors placement  = best' neighbors placement (head neighbors)
 
 main = do
-    let world = zip [1..] (map (\x->[x]) (shuffle nums (quot (length nums) 2)))
-    print  world
-    print  $ (anneal world 1)
+    -- print (best [(1,2),(9,4)] (2,3))
+    print placements
+    let foo = anneal placements []
+    print foo
+    print $ (anneal foo [])
+remove i xs = filter (\n -> n/=i) xs
+insert x (table, people) = (table, people++x)
+random x = mod  (x * 1103515245 + 12345) 1000000000  
