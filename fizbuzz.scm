1595b7dd959b5571f7cdb75788044e2217c165ec
John de la Garza <john@jjdev.com>
2016-04-16 11:17:42 -0700

    fizbuzz: fix spelling of divides?

--- fizbuzz.scm
+++ fizbuzz.scm
@@ -1,10 +1,10 @@
 (define (fizbuzz n)
-    (define (devides? x)
+    (define (divides? x)
         (= 0 (modulo n x)))
 
-    (if  (devides? 3) (display 'fiz))
-    (if  (devides? 5) (display 'buz))
-    (if  (or (devides? 3) (devides? 5)) (print)))
+    (if  (divides? 3) (display 'fiz))
+    (if  (divides? 5) (display 'buz))
+    (if  (or (divides? 3) (devides? 5)) (print)))
 
 
 (define (nums n)
